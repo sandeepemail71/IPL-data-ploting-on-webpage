7b4a5b7ebd0f75d9d8e0d2791b8cc6468a49b97c
Sandeep <sandeep.kumar@mountblue.io>
2018-05-28 06:01:04 +0000

    Revert "Merge branch 'stacked-bar' into 'master'"
    This reverts merge request !1

--- static/polls/test.js
+++ static/polls/test.js
@@ -1,7 +1,70 @@
+function plotMatchesPerYear(x, y) {
+  console.log(x);
+  console.log(y);
+  var chart = {
+    type: "column"
+  };
+  var title = {
+    text: "Matches per Year"
+  };
+  var subtitle = {
+    text: "Source: CSV file"
+  };
+  var xAxis = {
+    categories: x,
+    crosshair: true,
+    title: {
+        text: "Years",
+        margin: 10,
+        style: {
+            color: 'blue',
+            fontWeight: 'bold'
+        
+        }
+      }
+  };
+  var yAxis = {
+    min: 0,
+    title: {
+        text: "No of Matches",
+        margin: 10,
+        style: {
+            color: 'blue',
+            fontWeight: 'bold'
+        
+        }
+      }
+  };
 
+  var plotOptions = {
+    column: {
+      pointPadding: 0.05,
+      borderWidth: 0
+    }
+  };
+  var credits = {
+    enabled: false
+  };
+  var series = [
+    {
+      name: "Matches",
+      data: y
+    }
+  ];
 
+  var json = {};
+  json.chart = chart;
+  json.title = title;
+  json.subtitle = subtitle;
+  json.xAxis = xAxis;
+  json.yAxis = yAxis;
+  json.series = series;
+  json.plotOptions = plotOptions;
+  json.credits = credits;
+  return json;
+}
 
-function plotEconomicalBowler(x, y) {
+function economicalBowler(x, y) {
   console.log(x);
   console.log(y);
   var chart = {
@@ -29,13 +92,14 @@ function plotEconomicalBowler(x, y) {
   var yAxis = {
     min: 0,
     title: {
-      text: "Average Runs",
-      margin: 10,
-      style: {
-        color: "blue",
-        fontWeight: "bold"
+        text: "Average Runs",
+        margin: 10,
+        style: {
+            color: 'blue',
+            fontWeight: 'bold'
+        
+        }
       }
-    }
   };
 
   var plotOptions = {
@@ -66,7 +130,8 @@ function plotEconomicalBowler(x, y) {
   return json;
 }
 
-function plotExtraRun(x, y) {
+
+function extraRun(x, y) {
   console.log(x);
   console.log(y);
   var chart = {
@@ -129,134 +194,3 @@ function plotExtraRun(x, y) {
   json.credits = credits;
   return json;
 }
-
-
-
-function plotMatchesPerYear(x, y) {
-  console.log(x);
-  console.log(y);
-  var chart = {
-    type: "column"
-  };
-  var title = {
-    text: "Matches per Year"
-  };
-  var subtitle = {
-    text: "Source: CSV file"
-  };
-  var xAxis = {
-    categories: x,
-    crosshair: true,
-    title: {
-      text: "Years",
-      margin: 10,
-      style: {
-        color: "blue",
-        fontWeight: "bold"
-      }
-    }
-  };
-  var yAxis = {
-    min: 0,
-    title: {
-      text: "No of Matches",
-      margin: 10,
-      style: {
-        color: "blue",
-        fontWeight: "bold"
-      }
-    }
-  };
-
-  var plotOptions = {
-    column: {
-      pointPadding: 0.05,
-      borderWidth: 0
-    }
-  };
-  var credits = {
-    enabled: false
-  };
-  var series = [
-    {
-      name: "Matches",
-      data: y
-    }
-  ];
-
-  var json = {};
-  json.chart = chart;
-  json.title = title;
-  json.subtitle = subtitle;
-  json.xAxis = xAxis;
-  json.yAxis = yAxis;
-  json.series = series;
-  json.plotOptions = plotOptions;
-  json.credits = credits;
-  return json;
-}
-
-
-function plotMatchesWonByTeam(x, y, teams) {
-  var chart = {
-    type: "column"
-  };
-  var title = {
-    text: "Matches Won by each team in each season"
-  };
-  var subtitle = {
-    text: "Source: CSV file"
-  };
-  var xAxis = {
-    categories: x,
-    crosshair: true,
-    title: {
-      text: "Teams",
-      margin: 10,
-      style: {
-        color: "blue",
-        fontWeight: "bold"
-      }
-    }
-  };
-  var yAxis = {
-    min: 0,
-    title: {
-      text: "Matches",
-      margin: 10,
-      style: {
-        color: "blue",
-        fontWeight: "bold"
-      }
-    }
-  };
-
-  var plotOptions = {
-    series: {
-      stacking: "normal",
-      pointPadding: 0.05,
-      borderWidth: 0
-    }
-  };
-  var credits = {
-    enabled: false
-  };
-  var series = [];
-  for (var i = 0; i < teams.length; i++)
-    series.push({ name: teams[i], data: y[i] });
-  console.log(series);
-
-  var json = {};
-  json.chart = chart;
-  json.title = title;
-  json.subtitle = subtitle;
-  json.xAxis = xAxis;
-  json.yAxis = yAxis;
-  json.series = series;
-  json.plotOptions = plotOptions;
-  json.credits = credits;
-  return json;
-}
-
-
-
